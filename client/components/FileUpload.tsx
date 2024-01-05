f173ea9fffdadc816edef2ece2ff91c300804f91
shruti2522 <shruti.apc01@gmail.com>
2024-01-05 17:56:20 +0530

    csv file uploader added

---
+++ client/components/FileUpload.tsx
@@ -0,0 +1,47 @@
+import React, { useState, ChangeEvent } from 'react';
+import nextConfig from '../next.config';
+import axios from 'axios';
+
+interface FileUploadProps {
+  onUpload: (response: any) => void;
+}
+
+const FileUpload: React.FC<FileUploadProps> = ({ onUpload }) => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      console.error('No file selected');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await axios.post(nextConfig.NEXT_APP_BACKEND_URL+'/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      onUpload(response.data);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+};
+
+export default FileUpload;
